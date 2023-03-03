e17b4af3154f891c282d732c5a57e549d736756a
feerny <luisfpe19@gmail.com>
2023-03-03 12:31:06 -0500

    termine funciones finales y agrege envio de email(descomentar)

--- src/components/preguntasa/ComoQueMal.tsx
+++ src/components/preguntasa/ComoQueMal.tsx
@@ -5,8 +5,32 @@ import {
   IonGrid,
   IonRow,
 } from "@ionic/react";
+import emailjs from "@emailjs/browser";
 
 const  ComoQueMal: React.FC = () => {
+  const sendEmail=()=>{
+    // var templateParams = {
+    //   reply_to:"luisfpe219@gmail.com"
+
+    // };
+
+    // emailjs
+    //   .send(
+    //     `${process.env.REACT_APP_SERVICE_ID}`,
+    //     `${process.env.REACT_APP_TEMPLATE_ID}`,
+    //     templateParams,
+    //     "J19FWhSEGpLBBEG2H"
+    //   )
+    //   .then(
+    //     function (response) {
+    //       console.log("SUCCESS!", response.status, response.text);
+    //     },
+    //     function (error) {
+    //       console.log("FAILED...", error);
+    //     }
+    //   );
+      console.log("lelo");
+  }
     const showButtons=()=>{
         document.getElementById("button1")?.removeAttribute("disabled");
         document.getElementById("button1")?.classList.remove("disabled");
@@ -24,7 +48,7 @@ const  ComoQueMal: React.FC = () => {
         <IonGrid>
           <IonRow class="ion-justify-content-center rowDown">
             <IonCol size="11" >
-            <IonButton routerLink="/all-activities/YaNoEstesMal"  disabled  id="button1" className="disabled">Shi </IonButton>
+            <IonButton onClick={()=>{sendEmail()}} routerLink="/all-activities/YaNoEstesMal"  disabled  id="button1" className="disabled">Shi </IonButton>
             </IonCol>
             <IonCol size="9" offset="3">
             <IonButton routerLink="/InicioApp" disabled  id="button2" className="disabled2">Mentiris </IonButton>
--- src/components/preguntasa/YaNoEstesMal.tsx
+++ src/components/preguntasa/YaNoEstesMal.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { IonButton, IonCol, IonGrid, IonIcon, IonRow } from "@ionic/react";
-import { happy} from "ionicons/icons";
+import { happy } from "ionicons/icons";
+
 
 const YaNoEstesMal: React.FC = () => {
-    const showButtons=()=>{
 
-        document.getElementById("button3")?.removeAttribute("disabled");
-        document.getElementById("button3")?.classList.remove("disabled3");
-        document.getElementById("button3")?.classList.add("button3");
-  
-  
-      }
-      window.setTimeout(showButtons,3000);
+
+  const showButtons = () => {
+    document.getElementById("button3")?.removeAttribute("disabled");
+    document.getElementById("button3")?.classList.remove("disabled3");
+    document.getElementById("button3")?.classList.add("button3");
+  };
+  window.setTimeout(()=>{showButtons()}, 3000);
   return (
     <IonCol size="auto">
       <h1 id="h1Init4" className="h1Init4">
         Mira Tu Email
-        <IonIcon className="iconHappyBlack"   icon={happy} />
+        <IonIcon className="iconHappyBlack" icon={happy} />
       </h1>
       <IonGrid>
-
         <IonRow class="rowDown2">
           <IonCol size="11">
-            <IonButton routerLink="/InicioApp" disabled id="button3" className="disabled3">
+            <IonButton
+              routerLink="/InicioApp"
+              disabled
+              id="button3"
+              className="disabled3"
+            >
               Next
             </IonButton>
           </IonCol>
