b433db3c6d14f0c6a3afebed8db874d8cc2c565f
feerny <luisfpe19@gmail.com>
2023-03-14 09:46:31 -0500

    desmutee el audio para pruebas

--- src/pages/inicioApp/InicioApp.tsx
+++ src/pages/inicioApp/InicioApp.tsx
@@ -232,7 +232,6 @@ const InicioApp: React.FC<InicioAppProps> = (props) => {
         <ReactAudioPlayer
           src="https://www.bensound.com/bensound-music/bensound-pianomoment.mp3"
           autoPlay
-          muted
           loop
         />
       </IonContent>
