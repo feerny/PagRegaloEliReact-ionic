865d310aca6c2da55f00201e223ce011acd0cd21
feerny <luisfpe19@gmail.com>
2023-04-18 16:09:37 -0500

    optimize cambio de thema en barra de navegador

--- src/pages/appRegalo/AppRegalo.tsx
+++ src/pages/appRegalo/AppRegalo.tsx
@@ -17,7 +17,6 @@ import {
   IonTitle,
   IonToggle,
   IonToolbar,
-  useIonViewDidEnter,
 } from "@ionic/react";
 import {
   gift,
@@ -111,24 +110,10 @@ const AppRegalo: React.FC<AppRegaloProps> = (props) => {
   //funcion para modo oscuro
   const [darkmode, setdarkmode] = useState(false)
   const clickDarkTheme=(t:any)=>{
-    if (t.detail.checked===true) {
-      document?.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#222428")
-      setdarkmode(true)
-    }else{
-      document?.querySelector('meta[name="theme-color"]')?.setAttribute("content", "")
-      setdarkmode(false)
-    }
     localStorage.setItem("temeDark",`${t.detail.checked}`)
     setdarkmode(!darkmode)
-    
-    
   }
   useEffect(() => {
-    console.log("tema "+(darkmode?"dark":"default"));
-  }, [darkmode])
-  
-  useIonViewDidEnter(()=>{
-    
     if (localStorage.getItem("temeDark")==="true") {
       document?.querySelector('meta[name="theme-color"]')?.setAttribute("content", "#222428")
       setdarkmode(true)
@@ -136,7 +121,9 @@ const AppRegalo: React.FC<AppRegaloProps> = (props) => {
       document?.querySelector('meta[name="theme-color"]')?.setAttribute("content", "")
       setdarkmode(false)
     }
-  })
+    console.log("tema "+(darkmode?"dark":"default"));
+  }, [darkmode])
+
 
   return (
     <IonContent >
